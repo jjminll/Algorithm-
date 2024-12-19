faf2e1cc80ac64c86a3554aa0f214545fa5f3b44
jjminll <152157305+jjminll@users.noreply.github.com>
2024-12-19 19:19:49 +0900

    Delete 18.py

--- 18.py
+++
@@ -1,8 +0,0 @@
-def solution(brown, yellow):
-    total = brown + yellow 
-    
-    for h in range(1, total + 1):
-        if total % h == 0:
-            w = total // h
-            if (w-2)*(h-2) == yellow:
-                return [w, h]
